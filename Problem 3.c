5b269468e89e883494022b604f03e2d0e02d8a5d
Samarjeet shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2021-04-19 23:50:31 +0530

    Update Problem 3.c

--- Problem 3.c
+++ Problem 3.c
@@ -27,3 +27,38 @@ clue:-//If you want:-
     #include<stdio.h>
     #include<conio.h>
     
+/*  thoda clue:-
+copyy code and edit
+// Online C compiler to run C program on
+
+#include <stdio.h>
+
+int main()
+{
+ int i,j,n=4;
+ for(i=1;i<(n*2);i++){
+     for(j=1;j<(n*2);j++){
+         if(i==1||i==(n*2)-1||j==1||j==(n*2)-1){
+             printf("%d ",n);
+         }
+         if(i==2&&j>1&&j<(n*2)-1||i==(n*2)-2&&j>1&&j<(n*2)-1||j==2&&i>1&& i<(n*2)-1||j==(n*2)-2&&i>1&& i<(n*2)-1){
+             printf("%d ",n-1);
+         }
+         if(i==3&&j>1&&j<(n*2)-1||i==(n*2)-3&&j>1&&j<(n*2)-1||j==2&&i>2&&i<(n*2)-2||j==(n*2)-2&&i>1&& i<(n*2)-2){
+             //printf("%d ",n-1){
+             printf("%d ",n-2);
+         }
+         /*if(i==4&&j>1&&j<(n*2)-1||i==(n*2)-4&&j>1&&j<(n*2)-1){
+             printf("%d ",n-3);
+         }i*/
+         if(i==(n*2)/2&&j==(n*2)/2){
+             printf("%d ",n-3);
+         }
+         
+         
+         
+     }
+     printf("\n");
+ }
+ return 0;
+}*/
